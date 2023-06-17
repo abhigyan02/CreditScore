98b7818315119e6502cf12da2d16bdbfbc452935
Abhigyan Singh <abhi02gyan@gmail.com>
2023-06-17 19:05:12 +0530

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
@@ -8,6 +10,10 @@ credit_data = pd.read_csv('credit_data.csv')
 
 features = credit_data[['income', 'age', 'loan']]
 target = credit_data.default
+
+# machine learning handles arrays not data frames
+X = np.array(features).reshape(-1, 3)
+Y = np.array(target)
 
 # 30% of data set is for testing and 70% of data set is for training
 features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.3)
@@ -20,3 +26,7 @@ predictions = model.fit.predict(features_test)
 print("Confusion matrix: ")
 print(confusion_matrix(target_test, predictions))
 print('Accuracy percentage', accuracy_score(target_test, predictions)*100, "%")
+
+# using cross validation to predict a more general accuracy
+prediction = cross_validate(model, X, Y, cv=5)
+print("Cross Validation accuracy ", np.mean(prediction['test_score'])*100, "%")
